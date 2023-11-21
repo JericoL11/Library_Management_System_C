2bc31dc07749dab0cbd28b99797bc490cd4eefc5
JericoL <jericolabajo11@gmail.com>
2023-11-21 19:46:28 +0800

    Add project files.

---
+++ Library_Management_System_C/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Library_Management_System_C.Data;
+using Microsoft.AspNetCore.Mvc;
+//model class declaration
+using Library_Management_System_C.Models;
+namespace Library_Management_System_C.Controllers
+{
+    public class LoginController : Controller
+    {
+        //user fullname property
+        public string Fullname { get; set; }
+
+        //dapendcy injection
+        private readonly Library_Management_System_CContext _context;
+
+        public LoginController(Library_Management_System_CContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult LoginAct([Bind("Username, Password")] User user)
+        {
+            //Link to Database 
+            User loginuser = _context.User.Where(u => u.Username == user.Username && u.Password ==user.Password).FirstOrDefault();
+
+            //Verification
+            if(loginuser == null)
+            {
+                ModelState.AddModelError("", "Incorrect Username or Password");
+                return View("Index", user);
+            }
+            else
+            {
+                //assigning of property
+                Fullname = $"{loginuser.FirstName} {loginuser.LastName}";
+
+                return RedirectToAction("Index", "Users"/*,new { loginuser.id }*/);
+           
+            }
+        }
+    }
+}
---
+++ Library_Management_System_C/Data/Library_Management_System_CContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Library_Management_System_C.Models;
+
+namespace Library_Management_System_C.Data
+{
+    public class Library_Management_System_CContext : DbContext
+    {
+        public Library_Management_System_CContext (DbContextOptions<Library_Management_System_CContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Library_Management_System_C.Models.User> User { get; set; } = default!;
+    }
+}
